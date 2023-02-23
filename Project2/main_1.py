a71d2648de18cce752f11b6d07c6fd9c330f9c6d
tdefo <tzone1222@gmail.com>
2023-02-23 14:18:35 -0700

    Indentation and integration

--- Project2/main_1.py
+++ Project2/main_1.py
@@ -1,40 +1,74 @@
 import re
 import ast
 
-inputFile = 'testExamples.py'
+inputFile = 'Project2/testExamples.py'
+indentOutput = 'output.py'
 
-def findHeaders(inputPath, outputPath):
-  input = open(inputPath,encoding="utf8")
-  output = open(outputPath, "w", encoding="utf8")
 
-  for line in input.readlines():
-    # Parse and fix function headers
-    if line.startswith("def"):
-      header = "def " + checkAndFixHeader(line.replace("def", ""))
-      output.write(header)
-    else:
-      output.write(line)
-  input.close()
-  output.close()
+def check_indentation(inputFile):
+    with open(inputFile, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+
+    # Check and fix indentation errors
+    indentation_level = 1
+    updated_lines = []
+    for line in lines:
+        stripped_line = line.strip()
+        if stripped_line.startswith("if") or stripped_line.startswith("for") or stripped_line.startswith("#"):
+            updated_lines.append(
+                "    " * indentation_level + stripped_line + "\n")
+            indentation_level += 1
+        elif stripped_line.startswith("def"):
+            indentation_level = 0
+            updated_lines.append(stripped_line + "\n")
+            indentation_level += 1
+        elif stripped_line.startswith(""):
+            updated_lines.append(
+                "    " * indentation_level + stripped_line + "\n")
+        else:
+            updated_lines.append(
+                "    " * indentation_level + stripped_line + "\n")
+            indentation_level += 1
+
+    # Write the updated program to a new file
+    with open(indentOutput, 'w', encoding="utf8") as f:
+        f.writelines(updated_lines)
+
+
+def findHeaders(indentOutput, outputPath):
+    input = open(indentOutput, encoding="utf8")
+    output = open(outputPath, "w", encoding="utf8")
+
+    for line in input.readlines():
+        # Parse and fix function headers
+        if line.startswith("def"):
+            header = "def " + checkAndFixHeader(line.replace("def", ""))
+            output.write(header)
+        else:
+            output.write(line)
+    input.close()
+    output.close()
+
 
 def checkAndFixHeader(header):
-  specialChars = {"(":[], ")":[], ":":[], " ":[]}
-  # Parse header
-  for i in range(len(header) - 1, 0, -1):
-    char = header[i]
-    # Store indexes of special characters
-    if char in specialChars:
-      specialChars[char].append(i)
-      
-  if len(specialChars["("]) == 0:
-    header = header[:specialChars[" "][-1]] + "(" + header[specialChars[" "][-1] + 1:]
-  # add close parenthesis if none exist
-  if len(specialChars[")"]) == 0:
-    header = header[:specialChars[":"][0] - 1] + "):\n"
-  # add colon if none exist
-  if len(specialChars[":"]) == 0:
-      header = header[:specialChars[")"][0]] + "):\n"
-  return header.replace(" ", "")
+    specialChars = {"(": [], ")": [], ":": [], " ": []}
+    # Parse header
+    for i in range(len(header) - 1, 0, -1):
+        char = header[i]
+        # Store indexes of special characters
+        if char in specialChars:
+            specialChars[char].append(i)
+
+    if len(specialChars["("]) == 0:
+        header = header[:specialChars[" "][-1]] + \
+            "(" + header[specialChars[" "][-1] + 1:]
+    # add close parenthesis if none exist
+    if len(specialChars[")"]) == 0:
+        header = header[:specialChars[":"][0]] + "):\n"
+    # add colon if none exist
+    if len(specialChars[":"]) == 0:
+        header = header[:specialChars[")"][0]] + "):\n"
+    return header.replace(" ", "")
 
 
 """
@@ -44,6 +78,8 @@ It ignores the other possible print values, "print(", print in a function name.
 The tree has different names for items such as strings so this bypasses those because
 in the syntax tree they are not represeneted by their string value
 """
+
+
 class PrintCounter(ast.NodeVisitor):
     def __init__(self):
         self.count = 0
@@ -55,7 +91,7 @@ class PrintCounter(ast.NodeVisitor):
 
 
 def printCount(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding="utf8") as file:
         code = file.read()
 
     tree = ast.parse(code)
@@ -66,15 +102,16 @@ def printCount(filename):
 
 
 try:
-  printCount('print_test.py')
-  # TODO for function headers:
-  # - Parentheses syntax not finished:
-  #   - If there are 
-  # - Argument syntax is not checked
-  #   - Need to make sure there are commas if there is a space between arguments
-  #   - Check for this after the parseAndFixBrackets call
-  #
-  findHeaders(inputFile, "output.txt")
+    check_indentation(inputFile)
+    findHeaders(indentOutput, "output.txt")
+    printCount("output.txt")
+    # TODO for function headers:
+    # - Parentheses syntax not finished:
+    #   - If there are
+    # - Argument syntax is not checked
+    #   - Need to make sure there are commas if there is a space between arguments
+    #   - Check for this after the parseAndFixBrackets call
+    #
 
 finally:
-  print("complete")
+    print("complete")
